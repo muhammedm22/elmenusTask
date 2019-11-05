03334748bbc468fe5241db28551d6b2e978ffae5
Mohammed Mahmoud <m.mahmood@zadfresh.com>
2019-11-05 23:31:07 +0200

    Pods files

---
+++ Pods/Target Support Files/Pods-ElmenusTask/Pods-ElmenusTask-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ElmenusTask : NSObject
+@end
+@implementation PodsDummy_Pods_ElmenusTask
+@end
---
+++ Pods/Target Support Files/Pods-ElmenusTaskTests/Pods-ElmenusTaskTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ElmenusTaskTests : NSObject
+@end
+@implementation PodsDummy_Pods_ElmenusTaskTests
+@end
